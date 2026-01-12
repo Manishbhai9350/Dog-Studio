3d26eca48c403a5e32c1dd96de4f189ef40c35c5
Manishbhai9350 <keshraram294@gmail.com>
2026-01-12 21:03:18 +0530

    Tailwind Css Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import "./App.css";
 
 const App = () => {
   return (
-    <main className='w-screen h-auto'>
-      <section className="hero w-screen h-screen bg-black text-white">
-        <h1 className='text-green-600'>Dog Studio</h1>
+    <main className="w-screen h-auto px-6 pt-12">
+      <section className="hero flex flex-col justify-center items-start w-screen h-screen bg-black text-white">
+        <div className="heading w-full flex-1">
+          <h1 className="text-6xl sectra-light">
+          We <br />
+          Make <br />
+          Good <br />
+          Shit
+        </h1>
+        </div>
+        <div className="description w-full flex-1"></div>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
