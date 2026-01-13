edaa3c5a7697eef60d93d855e621ac1d157df02a
Manishbhai9350 <keshraram294@gmail.com>
2026-01-13 17:34:52 +0530

    Hero Section Dom and Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,42 @@ import { MdArrowForwardIos } from "react-icons/md";
 
 const App = () => {
   return (
-    <main className="w-screen text-white bg-black h-auto !pt-28">
-      <section className="hero flex flex-col justify-center items-start gap-10 lg:gap-36 w-screen h-auto">
-        <div className="heading !mx-8 lg:w-[45vw] flex-1 flex flex-col justify-center items-start text-7xl sectra-light lg:items-end lg:text-9xl">
+    <main className="w-screen overflow-x-hidden text-white bg-black h-auto md:!pt-20 max-md:!pt-28">
+      <section className="hero flex flex-col justify-center items-start gap-10 w-screen h-auto">
+        <div className="heading !mx-10 md:w-[47vw] md:leading-28 flex-1 flex flex-col justify-center items-start text-7xl sectra-light lg:items-end md:text-9xl">
           <h1>We</h1>
           <h1>Make</h1>
           <h1>Good</h1>
           <h1>Shit</h1>
         </div>
-        <div className="cursor-pointer lg:hidden hover:translate-x-1 transition-transform duration-200 showreel flex justify-start items-center gap-3 !mx-8">
+        <div className="cursor-pointer md:hidden hover:translate-x-1 transition-transform duration-200 showreel flex justify-start items-center gap-3 !mx-10">
           <div className="arrow text-rose-800">
             <MdArrowForwardIos size={15} className="font-semibold" />
           </div>
           <p className="text-md heebo-md">Our Showreel</p>
         </div>
-        <div className="description-container w-full h-fit flex justify-start items-start lg:justify-end">
-          <div className="description w-[45vw] flex flex-col justify-start items-start gap-6 !mx-8 flex-1">
-            <p className="t-2 heebo-light text-wrap tracking-[0.02rem]">
-              {" "}
-              Dogstudio is a multidisciplinary creative studio at the
-              intersection of art, design and technology.{" "}
-            </p>
-            <p className="text-secondary heebo-md text-sm">
-              Our goal is to deliver amazing experiences that make people talk,
-              and build strategic value for brands, tech, entertainment, arts &
-              culture.
-            </p>
-            <div className="handles mt-4 flex justify-start items-center gap-4 text-white text-xs">
-              <a href="#">Facebook</a>
-              <span>/</span>
-              <a href="#">Instagram</a>
-              <span>/</span>
-              <a href="#">Dribble</a>
-              <span>/</span>
-              <a href="#">Twitter</a>
+        <div className="description-container !mb-8 w-full h-fit flex justify-start items-start md:justify-end">
+          <div className="description-width w-full !mx-10 md:w-[50vw] lg:w-[35vw]  max-sm:flex-1">
+            <div className="description w-full md:!w-[350px] flex flex-col justify-start items-start gap-6">
+              <p className="text-2xl sm:text-[1.4rem] heebo-light text-wrap tracking-[0.02rem]">
+                {" "}
+                Dogstudio is a multidisciplinary creative studio at the
+                intersection of art, design and technology.{" "}
+              </p>
+              <p className="text-secondary heebo-md text-sm">
+                Our goal is to deliver amazing experiences that make people
+                talk, and build strategic value for brands, tech, entertainment,
+                arts & culture.
+              </p>
+              <div className="handles mt-4 md:!mt-12 flex justify-start items-center gap-4 text-white text-xs">
+                <a href="#">Facebook</a>
+                <span>/</span>
+                <a href="#">Instagram</a>
+                <span>/</span>
+                <a href="#">Dribble</a>
+                <span>/</span>
+                <a href="#">Twitter</a>
+              </div>
             </div>
           </div>
         </div>
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "640px",
+    //   md: "768px",
+      md: "400px", // Modified
+      lg: "1024px",
+    },
+  },
+  plugins: [],
+};
