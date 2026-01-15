a732b335d2bfaf09818e12e7a7ccae68580cec8f
Manishbhai9350 <keshraram294@gmail.com>
2026-01-15 19:29:58 +0530

    Projects Text Animation

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,28 +14,37 @@ const Project = ({ headline = "", title = "", description = "" }) => {
 
     if(!DescLines || !DescLines?.length) return;
 
+    gsap.killTweensOf(DescLines)
+
     if(hovered) {
-        gsap.fromTo(DescLines,{
-            y:30,
+      DescLines.forEach((line,i) => {
+        const rect = line.getBoundingClientRect()
+        if(!rect) return;
+        gsap.fromTo(line,{
+            y: 30,
             opacity:0,
         },{
-            delay:.3,
+            delay:.35 + .1/2 * (i + 1),
+            duration:.3,
             y:0,
-            stagger:.06,
             opacity:1
         })
+      })
     } else {
-        gsap.to(DescLines,{
-            y:-30,
-            opacity:0,
-            stagger:.06,
+      DescLines.forEach((line,i) => {
+        gsap.to(line,{
+          delay:.2 + .1/2 * (DescLines.length - (i + 1)),
+          y:-30,
+          opacity:0,
+          stagger:.06,
         })
+      })
     }
 
   },[hovered])
 
   return (
-    <div className="project w-full !my-14">
+    <div className="project w-full max-lg:!my-14">
       <div className="mobile lg:hidden w-full  flex gap-6 flex-col justify-center items-start">
         <div className="headline">
           <p className="text-xs uppercase sectra-light tracking-[.16rem] text-amber-500">
@@ -52,19 +61,19 @@ const Project = ({ headline = "", title = "", description = "" }) => {
         </div>
       </div>
       <div className="desktop max-lg:hidden w-full cursor-pointer group">
-        <h1 onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="text-7xl heebo-v font-[100] opacity-40 group-hover:opacity-100 !transition-opacity !duration-200">
+        <h1 onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="text-7xl heebo-v font-[100] opacity-40 group-hover:opacity-100 !transition-opacity !duration-200 lg:!py-7">
           {title}
         </h1>
         <div
           ref={container}
-          className="description fixed bottom-12 right-12 w-[300px]"
+          className="description pointer-events-none fixed bottom-12 right-12 w-full"
         >
-          <p className="desktop-description-line relative text-right w-full text-xs !mb-4 sectra-light uppercase tracking-[.2em] text-amber-600">
+          <p className="desktop-description-line opacity-0 relative text-right w-full text-xs !mb-4 sectra-light uppercase tracking-[.2em] text-amber-600">
             {headline}
           </p>
 
           {lines.map((line, i) => (
-            <p key={i} className="desktop-description-line relative text-xl heebo-light w-full text-right">
+            <p key={i} className="desktop-description-line opacity-0 relative text-xl heebo-light w-full text-right">
               {line}
             </p>
           ))}
